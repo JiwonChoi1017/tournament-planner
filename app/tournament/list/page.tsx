f64a03d10e8f44f4dfbb6e2d3e4e56c6945c3c97
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2025-09-17 22:49:47 +0900

    Supabase Clientを作成

--- app/tournament/list/page.tsx
+++ app/tournament/list/page.tsx
@@ -1,9 +1,16 @@
-const TournamentList = () => {
+import { createClient } from "@/utils/supabase/server";
+
+export default async function TournamentList() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("aaa").select();
+
+  if (error) {
+    console.error(error);
+  }
+
   return (
     <>
       <span>Tournament List</span>
     </>
   );
-};
-
-export default TournamentList;
+}
