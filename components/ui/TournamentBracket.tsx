08c21bb8ba2eef11cdf174be131833f114c90f5e
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2025-09-19 22:06:43 +0900

    トーナメント表作成画面追加（途中）

--- components/ui/TournamentBracket.tsx
+++ components/ui/TournamentBracket.tsx
@@ -1,5 +1,41 @@
-const TournamentBracket = () => {
-  return <div>Bracket</div>;
+import { MATCH_RESULT } from "@/constants/commonConstant";
+import { Match } from "@/types/common";
+
+type Props = {
+  matches: Match[];
+};
+
+const TournamentBracket = ({ matches }: Props) => {
+  return (
+    <div>
+      {matches.map(({ round, player1, player2 }) => (
+        <div key={round}>
+          {player1 && (
+            <div>
+              <input
+                type="radio"
+                name={`result_${round}`}
+                value={player1.id}
+                defaultChecked={player1.result === MATCH_RESULT.WIN}
+              />
+              <label>{`${player1.name}`}</label>
+            </div>
+          )}
+          {player2 && (
+            <div>
+              <input
+                type="radio"
+                name={`result_${round}`}
+                value={player2.id}
+                defaultChecked={player2.result === MATCH_RESULT.WIN}
+              />
+              <label>{`${player2.name}`}</label>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TournamentBracket;
--- types/common.d.ts
+++ types/common.d.ts
@@ -1,3 +1,19 @@
-import { AUTH_PROVIDER } from "@/constants/commonConstant";
+import { AUTH_PROVIDER, MATCH_RESULT } from "@/constants/commonConstant";
 
 type AuthProvider = (typeof AUTH_PROVIDER)[keyof typeof AUTH_PROVIDER];
+
+type MatchResult = (typeof MATCH_RESULT)[keyof typeof MATCH_RESULT];
+
+type Player = {
+  id: number;
+  entryNumber?: number;
+  name: string;
+  nameKana?: string;
+  hometown?: string;
+};
+
+type Match = {
+  round: number;
+  player1?: Player & { score?: number; color?: string; result?: MatchResult };
+  player2?: Player & { score?: number; color?: string; result?: MatchResult };
+};
