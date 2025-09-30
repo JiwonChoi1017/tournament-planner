e9d7840489e58586ce8218efc884edfbcffca004
Jiwon Choi <jiwonchoi@JiwonnoMacBook-Pro.local>
2025-09-30 17:35:58 +0900

    トーナメント関連ロジックのリファクタリング

--- components/ui/TournamentBracket.tsx
+++ components/ui/TournamentBracket.tsx
@@ -1,56 +1,62 @@
 import { MATCH_RESULT } from "@/constants/commonConstant";
 import { Matches } from "@/types/common";
+import React from "react";
 
 type Props = {
   matches: Matches | undefined;
 };
 
-const TournamentBracket = ({ matches }: Props) => {
-  if (!matches) return;
+const TournamentBracket = React.memo(({ matches }: Props) => {
   return (
     <div>
-      {Array.from(matches.entries()).map(([id, matchList]) => (
-        <div key={id}>
-          {matchList.map(({ currentRound, player1, player2 }, idx) => (
-            <div key={`${id}_${idx}`} style={{ marginBottom: "10px" }}>
-              {(player1 || id > 0) && (
-                <div>
-                  <input
-                    type="radio"
-                    name={`result_${id}_${idx}`}
-                    value={player1?.id}
-                    defaultChecked={player1?.result === MATCH_RESULT.WIN}
-                  />
-                  <label>
-                    <input
-                      type="text"
-                      defaultValue={`${player1?.name ?? ""}`}
-                    />
-                  </label>
+      {matches ? (
+        <>
+          {Array.from(matches.entries()).map(([id, matchList]) => (
+            <div key={id}>
+              {matchList.map(({ currentRound, player1, player2 }, idx) => (
+                <div key={`${id}_${idx}`} style={{ marginBottom: "10px" }}>
+                  {(player1 || id > 0) && (
+                    <div>
+                      <input
+                        type="radio"
+                        name={`result_${id}_${idx}`}
+                        value={player1?.id}
+                        defaultChecked={player1?.result === MATCH_RESULT.WIN}
+                      />
+                      <label>
+                        <input
+                          type="text"
+                          defaultValue={`${player1?.name ?? ""}`}
+                        />
+                      </label>
+                    </div>
+                  )}
+                  {(player2 || id > 0) && (
+                    <div>
+                      <input
+                        type="radio"
+                        name={`result_${id}_${idx}`}
+                        value={player2?.id}
+                        defaultChecked={player2?.result === MATCH_RESULT.WIN}
+                      />
+                      <label>
+                        <input
+                          type="text"
+                          defaultValue={`${player2?.name ?? ""}`}
+                        />
+                      </label>
+                    </div>
+                  )}
                 </div>
-              )}
-              {(player2 || id > 0) && (
-                <div>
-                  <input
-                    type="radio"
-                    name={`result_${id}_${idx}`}
-                    value={player2?.id}
-                    defaultChecked={player2?.result === MATCH_RESULT.WIN}
-                  />
-                  <label>
-                    <input
-                      type="text"
-                      defaultValue={`${player2?.name ?? ""}`}
-                    />
-                  </label>
-                </div>
-              )}
+              ))}
             </div>
           ))}
-        </div>
-      ))}
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
-};
+});
 
 export default TournamentBracket;
